d01d27e382463db2470e801d0f1584be15acad49
mohamed oussama <mohamedoussama.bougueddach@gmail.com>
2022-10-24 19:07:25 +0100

    Implement navigation to the main screen after logging in, and presist login state

--- app/src/main/java/com/example/travelblog/LoginActivity.kt
+++ app/src/main/java/com/example/travelblog/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.travelblog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
@@ -12,10 +14,17 @@ import com.google.android.material.textfield.TextInputLayout
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private val preferences: BlogPreferences by lazy {
+        BlogPreferences(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        if(preferences.isLoggedIn()) {
+            startMainActivity()
+            finish()
+            return
+        }
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -57,10 +66,15 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun performLogin() {
+        preferences.setLoggedIn(true)
         binding.loginButton.visibility = View.INVISIBLE
         binding.progressBar.visibility = View.VISIBLE
         binding.textUsernameLayout.isEnabled = false
         binding.textPasswordInput.isEnabled = false
+        Handler().postDelayed({
+            startMainActivity()
+            finish()
+        }, 2000)
     }
 
     private fun showErrorDialog() {
@@ -69,5 +83,10 @@ class LoginActivity : AppCompatActivity() {
             .setMessage("The username or password is invalid, please try again")
             .setPositiveButton("OK") { dialog, which -> dialog.dismiss() }
             .show()
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
